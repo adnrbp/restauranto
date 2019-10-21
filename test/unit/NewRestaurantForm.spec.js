b8bcaa24244a49c2c7575de1d2edd20beb1fe7ac
adnrbp <adnrbp@users.noreply.github.com>
2019-10-20 21:54:31 -0500

    [Upd] Restaurant Form: clear input

--- test/unit/NewRestaurantForm.spec.js
+++ test/unit/NewRestaurantForm.spec.js
@@ -4,11 +4,15 @@ import NewRestaurantForm from '../../src/components/NewRestaurantForm';
 
 describe('NewRestaurantForm', () => {
   describe('clicking the save button', () => {
-    it('calls the onSave handler', () => {
-      const saveHandler = jest.fn();
+    let saveHandler;
+    let wrapper;
+    let restaurantName = 'Delicious Food';
 
-      const wrapper = mount(<NewRestaurantForm onSave={saveHandler}/>);
-      const restaurantName = 'Delicious Food';
+    beforeEach(() => {
+      saveHandler = jest.fn();
+
+      wrapper = mount(<NewRestaurantForm onSave={saveHandler}/>);
+
       wrapper
         .find('input[data-test="newRestaurantName"]')
         .simulate('change', { target: { value: restaurantName } });
@@ -16,8 +20,18 @@ describe('NewRestaurantForm', () => {
       wrapper
         .find('button[data-test="saveNewRestaurantButton"]')
         .simulate('click');
+    });
 
+    it('calls the onSave handler', () => {
       expect(saveHandler).toHaveBeenCalledWith(restaurantName);
+    });
+
+    it('clears the text field', () => {
+      expect(
+        wrapper
+          .find('input[data-test="newRestaurantName"]')
+          .props().value,
+      ).toEqual('');
     });
   });
 });
